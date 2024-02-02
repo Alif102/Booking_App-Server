bcecb7e1bc9a172f684ad0e9053976aa75511276
Alif102 <alifahmed102@gmail.com>
2024-02-02 19:12:35 +0600

    get user role

---
+++ index.js
@@ -0,0 +1,237 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const jwt = require('jsonwebtoken')
+const morgan = require('morgan')
+const port = process.env.PORT || 5000
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
+const nodemailer = require('nodemailer');
+
+// middleware
+app.use(cors({
+  origin : [
+     'http://localhost:5173', 'https://hotel-booking-mern-c8edc.web.app' , 'https://hotel-booking-mern-c8edc.firebaseapp.com'
+  ],
+  credentials: true
+ }));
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan('dev'))
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token
+  console.log(token)
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+    if (err) {
+      console.log(err)
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded
+    next()
+  })
+}
+
+
+// Send email
+const sendEmail = (emailAddress, emailData) => {
+  //Create a transporter
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.MAIL,
+      pass: process.env.PASS,
+    },
+  })
+
+  //verify connection
+  transporter.verify((error, success) => {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('Server is ready to take our emails', success)
+    }
+  })
+
+  const mailBody = {
+    from: process.env.MAIL,
+    to: emailAddress,
+    subject: emailData?.subject,
+    html: `<p>${emailData?.message}</p>`,
+  }
+
+  transporter.sendMail(mailBody, (error, info) => {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('Email sent: ' + info.response)
+    }
+  })
+}
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.30z9ip6.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+async function run() {
+  try {
+    // const bookingCollection = client.db('Booking').collection('bookings')
+    const usersCollection = client.db('Booking').collection('users')
+   const roomsCollection = client.db('Booking').collection('hotelrooms')
+   const bookingsCollection = client.db('Booking').collection('bookingsRooms')
+
+
+
+
+    // Role verification middlewares
+    // For admins
+    const verifyAdmin = async (req, res, next) => {
+      const user = req.user
+      console.log('user from verify admin', user)
+      const query = { email: user?.email }
+      const result = await usersCollection.findOne(query)
+      if (!result || result?.role !== 'admin')
+        return res.status(401).send({ message: 'unauthorized access' })
+      next()
+    }
+
+    // For hosts
+    const verifyHost = async (req, res, next) => {
+      const user = req.user
+      const query = { email: user?.email }
+      const result = await usersCollection.findOne(query)
+      if (!result || result?.role !== 'host')
+        return res.status(401).send({ message: 'unauthorized access' })
+      next()
+    }
+    // auth related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      console.log('I need a new jwt', user)
+      const token = jwt.sign(user, process.env.SECRET_TOKEN, {
+        expiresIn: '365d',
+      })
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true })
+    })
+
+    // Logout
+    app.get('/logout', async (req, res) => {
+      try {
+        res
+          .clearCookie('token', {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          })
+          .send({ success: true })
+        console.log('Logout successful')
+      } catch (err) {
+        res.status(500).send(err)
+      }
+    })
+
+        // Save or modify user email, status in DB
+        app.put('/users/:email', async (req, res) => {
+          const email = req.params.email
+          const user = req.body
+          const query = { email: email }
+          const options = { upsert: true }
+          const isExist = await usersCollection.findOne(query)
+          console.log('User found?----->', isExist)
+          if (isExist) {
+            if (user?.status === 'Requested') {
+              const result = await usersCollection.updateOne(
+                query,
+                {
+                  $set: user,
+                },
+                options
+              )
+              return res.send(result)
+            } else {
+              return res.send(isExist)
+            }
+          }
+          const result = await usersCollection.updateOne(
+            query,
+            {
+              $set: { ...user, timestamp: Date.now() },
+            },
+            options
+          )
+          res.send(result)
+        })
+    
+    
+
+    // Get user role
+    app.get('/user/:email', async (req, res) => {
+      const email = req.params.email
+      const result = await usersCollection.findOne({ email })
+      res.send(result)
+    })
+
+     // Get all rooms
+     app.get('/rooms', async (req, res) => {
+      const result = await roomsCollection.find().toArray()
+      res.send(result)
+    })
+
+    //get rooms for host
+    app.get('/rooms/:email', async (req, res) => {
+      const email = req.params.email
+      const result = await roomsCollection
+        .find({ 'host.email': email })
+        .toArray()
+      res.send(result)
+    })
+
+    // Get single room data
+    app.get('/room/:id', async (req, res) => {
+      const id = req.params.id
+      const result = await roomsCollection.findOne({ _id: new ObjectId(id) })
+      res.send(result)
+    })
+
+    
+
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('Hello from StayVista Server..')
+})
+
+app.listen(port, () => {
+  console.log(`StayVista is running on port ${port}`)
+})
